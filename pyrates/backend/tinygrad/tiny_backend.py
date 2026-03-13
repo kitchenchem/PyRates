ae28fb4d9dce52c3e5e8767689c837932498ca42
Nick Janapol <njanapol@protonmail.com>
2026-03-13 15:27:20 -0700

    slow tinybackend

--- pyrates/backend/tinygrad/tiny_backend.py
+++ pyrates/backend/tinygrad/tiny_backend.py
@@ -35,12 +35,11 @@ from ..computegraph import ComputeVar
 from .tiny_funcs import tiny_funcs
 
 # external _imports
-import tinygrad
+from tinygrad import Tensor, dtypes
 import numpy as np
 from typing import Callable, Optional, Dict, List
 
 # meta infos
-__author__ = "Nick Janapol"
 __status__ = "development"
 
 
@@ -48,4 +47,87 @@ __status__ = "development"
 # classes for backend functionalities #
 #######################################
 
-class TinyBackend(BaseBackend): ...
+class TinyBackend(BaseBackend):
+
+    def __init__(self,
+                 ops: Optional[Dict[str, str]] = None,
+                 imports: Optional[List[str]] = None,
+                 **kwargs
+                 ) -> None:
+        """Instantiates tinygrad backend.
+        """
+
+        # add user-provided operations to function dict
+        tiny_ops = tiny_funcs.copy()
+        if ops:
+            tiny_ops.update(ops)
+
+        # call parent method
+        super().__init__(ops=tiny_ops, imports=imports, **kwargs)
+        self._imports[0] = "from math import pi, sqrt"
+
+    def get_var(self, v: ComputeVar):
+        return Tensor(super().get_var(v).tolist())
+
+    @staticmethod
+    def _solve_euler(func: Callable, args: tuple, T: float, dt: float, dts: float, y: Tensor, t0: Tensor
+                     ) -> np.ndarray:
+
+        # preparations for fixed step-size integration
+        t0 = int(t0.item()) if isinstance(t0, Tensor) else int(t0)
+        idx = 0
+        steps = int(np.round(T / dt))
+        store_steps = int(np.round(T / dts))
+        store_step = int(np.round(dts / dt))
+        state_rec = np.zeros((store_steps, y.shape[0]) if y.shape else (store_steps, 1), dtype=np.float32)
+
+        # solve ivp for forward Euler method
+        for step in range(t0, steps + t0):
+            if step % store_step == t0:
+                state_rec[idx, :] = y.numpy()
+                idx += 1
+            rhs = func(step, y, *args)
+            y = y + dt * rhs
+
+        return state_rec
+
+    @staticmethod
+    def _solve_heun(func: Callable, args: tuple, T: float, dt: float, dts: float, y: Tensor, t0: Tensor
+                    ) -> np.ndarray:
+
+        # preparations for fixed step-size integration
+        t0 = int(t0.item()) if isinstance(t0, Tensor) else int(t0)
+        idx = 0
+        steps = int(np.round(T / dt))
+        store_steps = int(np.round(T / dts))
+        store_step = int(np.round(dts / dt))
+        state_rec = np.zeros((store_steps, y.shape[0]) if y.shape else (store_steps, 1), dtype=np.float32)
+
+        # solve ivp for Heun's method
+        for step in range(t0, steps + t0):
+            if step % store_step == t0:
+                state_rec[idx, :] = y.numpy()
+                idx += 1
+            rhs = func(step, y, *args)
+            y_0 = y + dt * rhs
+            y = y + dt / 2 * (rhs + func(step, y_0, *args))
+
+        return state_rec
+
+    @staticmethod
+    def _solve_scipy(func: Callable, args: tuple, T: float, dt: float, y: Tensor, t0: Tensor,
+                     times: np.ndarray, **kwargs) -> np.ndarray:
+
+        # solve ivp via scipy methods
+        from scipy.integrate import solve_ivp
+        t0 = float(t0.item()) if isinstance(t0, Tensor) else float(t0)
+        kwargs['t_eval'] = times
+
+        # wrapper to rhs function
+        def f(t, y_np):
+            rhs = func(Tensor([t]), Tensor(y_np.tolist()), *args)
+            return rhs.numpy()
+
+        # call scipy solver
+        results = solve_ivp(fun=f, t_span=(t0, T), y0=y.numpy(), first_step=dt, **kwargs)
+        return results['y'].T
